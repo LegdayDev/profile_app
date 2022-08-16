f19959fbb56b0fb506ef296a02414279113ae033
jaeyoung <chlwodud0327@naver.com>
2022-08-16 09:56:03 +0900

    complete commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/components/profile_buttons.dart';
+import 'package:flutter_profile/components/profile_count_info.dart';
+import 'package:flutter_profile/components/profile_header.dart';
+import 'package:flutter_profile/components/profile_tab.dart';
+import 'package:flutter_profile/pages/profile_page.dart';
+import 'package:flutter_profile/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
